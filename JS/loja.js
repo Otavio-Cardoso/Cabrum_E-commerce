fa9396c47eb453706e7d6344e6d9bba4bb9990d7
Cardoso <ocardoso057@gmail.com>
2023-05-19 10:25:13 -0300

    pt2

--- JS/loja.js
+++
@@ -1,9 +0,0 @@
-let fone = document.getElementById("comprar_fone");
-let bateria = document.getElementById("comprar_bateria");
-
-fone.addEventListener("click", compra);
-bateria.addEventListener("click", compra);
-
-function compra() {
-    alert("Compra realizada com sucesso!");
-}
