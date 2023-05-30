c0580330f59ee394cec2cfbe7c93506f243bd283
Cardoso <ocardoso057@gmail.com>
2023-05-30 15:53:21 -0300

    Adição da funcionalidade do carrinho

--- JS/loja.js
+++ JS/loja.js
@@ -1,68 +1,90 @@
 let produtos = [
-  {id:1, image:"<img src=\"../images/fone_bluetooth.png\" width=\"100\"/>", produto:"Fone TWS Denovo", descricao:"Fone Bluetooth TWS, com certificação IPX8", preco:159.90, qtde:10},
-  {id:2, image:"<img src=\"../images/carregador_portatil.webp\" width=\"100\"/>", produto:"PowerBank tuVerde", descricao:"Power Bank de 12.000mAh, com entrada USB-C e 2 entradas USB-A", preco:99.90, qtde:15}
+  { id: 0, image: "<img src=\"../images/fone_bluetooth.png\" width=\"100\"/>", produto: "Fone TWS Denovo", descricao: "Fone Bluetooth TWS, com certificação IPX8", preco: 159.90, qtde: 10 },
+  { id: 1, image: "<img src=\"../images/carregador_portatil.webp\" width=\"100\"/>", produto: "PowerBank tuVerde", descricao: "Power Bank de 12.000mAh, com entrada USB-C e 2 entradas USB-A", preco: 99.90, qtde: 15 }
 ];
+
 let carrinho = JSON.parse(localStorage.getItem('carrinho')) || [];
 
-if (localStorage.getItem('mercadorias') != null) {
-  produtos = [];
-  produtos = JSON.parse(localStorage.getItem('mercadorias'));
-} else {
-  localStorage.setItem("mercadorias", JSON.stringify(produtos));
+function renderizarCarrinho() {
+  let carrinhoElement = document.getElementById("itens-carrinho");
+  let totalElement = document.getElementById("total");
+  let total = 0;
+
+  if (!carrinhoElement) {
+    console.error("Elemento 'itens-carrinho' não encontrado.");
+    return;
+  }
+
+  carrinhoElement.innerHTML = "";
+
+  if (carrinho.length === 0) {
+    carrinhoElement.innerHTML = "<li>Nenhum item no carrinho.</li>";
+  } else {
+    for (let i = 0; i < carrinho.length; i++) {
+      let item = carrinho[i];
+
+      let itemElement = document.createElement("li");
+      itemElement.innerHTML = `${item.produto} - R$${item.preco_unit.toFixed(2)} x ${item.quantidade}`;
+      carrinhoElement.appendChild(itemElement);
+
+      total += item.preco_unit * item.quantidade;
+    }
+  }
+
+  if (totalElement) {
+    totalElement.innerHTML = `Total: R$${total.toFixed(2)}`;
+  } else {
+    console.error("Elemento 'total' não encontrado.");
+  }
+
+  localStorage.setItem("carrinho", JSON.stringify(carrinho));
 }
 
-let corpo_tabela = document.getElementById("corpo_tabela");
+function adicionarCarrinho(id) {
+  let idProd = parseInt(id);
+  let produto = produtos.find(p => p.id === idProd);
 
-for (let i = 0; i < produtos.length; i++) {
-  //comando para criar linha
-	let linha = document.createElement("tr");
-  linha.id = "produto" + (i + 1);
+  if (produto) {
+    let itemCarrinho = carrinho.find(item => item.id === idProd);
 
-  //comando para criar um coluna
-  //comando para colocar uma informação na coluna
-  //comando para colocar as colunas na linha.
-	let imagem = document.createElement("td");
-  imagem.innerHTML = produtos[i].image;
-  linha.appendChild(imagem);
- 
-  let nome_prod = document.createElement("td");
-  nome_prod.innerHTML = produtos[i].produto;
-  linha.appendChild(nome_prod);
+    if (itemCarrinho) {
+      itemCarrinho.quantidade++;
+      itemCarrinho.preco_tot = itemCarrinho.preco_unit * itemCarrinho.quantidade;
+    } else {
+      itemCarrinho = {
+        id: produto.id,
+        produto: produto.produto,
+        preco_unit: produto.preco,
+        quantidade: 1,
+        preco_tot: produto.preco
+      };
+      carrinho.push(itemCarrinho);
+    }
 
-  let descricao_produto = document.createElement("td");
-	descricao_produto.innerHTML = produtos[i].descricao;
-	linha.appendChild(descricao_produto);
-
-  let preco_produto = document.createElement("td");
-	preco_produto.innerHTML = "R$" + produtos[i].preco;
-	linha.appendChild(preco_produto);
-
-  let coluna_botao = document.createElement("td");
-	let botao = document.createElement("input");
-
-  botao.id = "bt" + i;
-  botao.type = "button";
-  botao.value = "Comprar";
-
-  coluna_botao.appendChild(botao);
-  linha.appendChild(coluna_botao);
-
-  botao = addEventListener("click", function() {adicionar_carrinho(botao.value)}); 
- 
-  //comando para colocar a linha no corpo da tabela
-  corpo_tabela.appendChild(linha);
+    renderizarCarrinho();
+  }
 }
 
-function adicionar_carrinho(id_bt) {
-  let id_prod = id_bt.substring(2, id_bt.length);
-  id_prod = parseInt(id_prod);
-  let prod_carrinho = {
-                  id:produtos[id_prod].id,
-                  nome:produtos[id_prod].produto,
-                  preco_unit:produtos[id_prod].preco,
-                  quantidade: qtde, 
-                  preco_tot: prod_carrinho.preco * prod_carrinho.qtde
-  };
+function removerCarrinho(id) {
+  let idProd = parseInt(id);
+  let itemCarrinho = carrinho.find(item => item.id === idProd);
 
-  carrinho.push(prod_carrinho);
+  if (itemCarrinho) {
+    itemCarrinho.quantidade--;
+
+    if (itemCarrinho.quantidade === 0) {
+      carrinho = carrinho.filter(item => item.id !== idProd);
+    }
+
+    renderizarCarrinho();
+  }
 }
+
+function limparCarrinho() {
+  carrinho = [];
+  renderizarCarrinho();
+}
+
+window.addEventListener("DOMContentLoaded", function () {
+  renderizarCarrinho();
+});
