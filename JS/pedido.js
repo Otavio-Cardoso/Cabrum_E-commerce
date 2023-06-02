4cf28642a5e591212f85a50ce662423adb0797e0
Diogo Timóteo <dtimoteo292@gmail.com>
2023-06-02 19:11:37 -0300

    Mudanças no logo, melhora no JS

--- JS/pedido.js
+++ JS/pedido.js
@@ -11,13 +11,8 @@ let txt_cidade = document.getElementById("cidade");
 let txt_estado = document.getElementById("estado");
 
 let bt_finalizar = document.getElementById("btn_finalizar");
-bt_finalizar.addEventListener("click", function() {finalizarCompra();});
 
-let infos_pessoas = {
-                    cpf: txt_cpf.value, nome: txt_nome.value, email: txt_email.value,
-                    cep: txt_cep.value, logradouro: txt_logradouro.value, numero: txt_numero.value,
-                    bairro: txt_bairro.value, cidade: txt_cidade.value, estado: txt_estado.value
-};
+bt_finalizar.addEventListener("click", function() {finalizarCompra();});
 
 // Função para completar os oustro inputs text caso tenha um cpf no local storage
 function auto_completar() {
@@ -26,47 +21,48 @@ function auto_completar() {
   }
 }
 
-function zerarPedidos() {
-  localStorage.removeItem("pedidos");
-  exibirPedidos();
-}
-
 function finalizarCompra() {
-  let carrinho = JSON.parse(localStorage.getItem("carrinho")) || [];
-  let total = 0;
+  let infos_pessoas = {
+    cpf: txt_cpf.value, nome: txt_nome.value, email: txt_email.value,
+    cep: txt_cep.value, logradouro: txt_logradouro.value, numero: txt_numero.value,
+    bairro: txt_bairro.value, cidade: txt_cidade.value, estado: txt_estado.value
+  };
 
-  // Calcular o total da compra
-  for (let i = 0; i < carrinho.length; i++) {
-    let item = carrinho[i];
-    total += item.preco_unit * item.quantidade;
-  } 
-  
-  if (total == 0) {
-    alert("Impossível finalizar o pedido com o carrinho vazio.");
+  if ((infos_pessoas.cpf === "") || (infos_pessoas.nome === "") || (infos_pessoas.email === "") || (infos_pessoas.cep === "") || (infos_pessoas.logradouro === "") 
+    || (infos_pessoas.numero === "") || (infos_pessoas.bairro === "") || (infos_pessoas.cidade === "") || (infos_pessoas.estado === "")) {
+    alert("Dados não preenchidos completamente");
   } else {
-    // Gerar um ID único para o pedido
-    let pedidoId = new Date().getTime();
+    let carrinho = JSON.parse(localStorage.getItem("carrinho")) || [];
+    let total = 0;
 
-    // Criar o objeto do pedido
-    let pedido = {
-      id: pedidoId,
-      total: total,
-      itens: carrinho
-    };
+    // Calcular o total da compra
+    for (let i = 0; i < carrinho.length; i++) {
+      let item = carrinho[i];
+      total += item.preco_unit * item.quantidade;
+    } 
+  
+    if (total == 0) {
+      alert("Impossível finalizar o pedido com o carrinho vazio.");
+    } else {
+      // Gerar um ID único para o pedido
+      let pedidoId = new Date().getTime();
+
+      // Criar o objeto do pedido
+      let pedido = {
+        id: pedidoId,
+        total: total,
+        itens: carrinho
+      };
     
-    let clientes = [];
-    clientes.push([infos_pessoas, pedido]);
-    localStorage.setItem("clientes", JSON.stringify(clientes));
+      let clientes = JSON.parse(localStorage.getItem("clientes")) || [];
+      clientes.push([infos_pessoas, pedido]);
+      localStorage.setItem("clientes", JSON.stringify(clientes));
 
-    // Armazenar o pedido no localStorage
-    let pedidos = JSON.parse(localStorage.getItem("pedidos")) || [];
-    pedidos.push(pedido);
-    localStorage.setItem("pedidos", JSON.stringify(pedidos));
+      // Limpar o carrinho
+      localStorage.removeItem("carrinho");
 
-    // Limpar o carrinho
-    localStorage.removeItem("carrinho");
-
-    // Redirecionar para a página de pedidos
-    window.location.href = "pedidos.html";
+      // Redirecionar para a página de pedidos
+      window.location.href = "../HTML/pag_inicial.html";
+    }
   }
 }
